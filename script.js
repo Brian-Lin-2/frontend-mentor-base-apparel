01bb29ff692451e8519b30406a4a9dbd55a8251c
Brian <linbrian24@gmail.com>
2023-04-06 20:45:44 -0400

    Add error state in JS

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-const email = document.getElementById('email');
-const userEmail = document.getElementById('email__text');
+const email = document.querySelector(".email");
+const userEmail = document.querySelector(".email__text");
+const input = document.querySelector(".email__input");
+
+// Error states.
+const errorIcon = document.querySelector(".email__errorIcon");
+const errorMessage = document.querySelector(".email__errorMessage");
+
+// Imported regex expression.
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
 
 // When form is clicked, determine if email is valid.
 email.addEventListener('submit', (e) => {
-    // Stops the page from refreshing.
-    e.preventDefault();
-
-    // Use .value() for input elements. Sort of like .textContent().
     console.log(userEmail.value);
+    // Determine if email is valid;
+    // Use .value() for input elements. Sort of like .textContent().
+    if (!validateEmail(userEmail.value)) {
+        input.classList.add("email__error");
+        errorIcon.classList.remove("hide");
+        errorMessage.classList.remove("hide");
+
+
+        // Stops the page from refreshing.
+        e.preventDefault();
+    }
 });
